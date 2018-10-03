49e53f907e42bc4e95f743194a47df1b8a7996fb
niklas <niklas.ederot-magnusson@sogeti.se>
2018-10-03 20:05:31 +0200

    init asp.net core app with angular

---
+++ Gibraltar/Gibraltar.Web/Controllers/DogController.cs
@@ -0,0 +1,43 @@
+﻿using Gibraltar.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gibraltar.Web.Controllers
+{
+    [Route("api/v1/dog")]
+    public class DogController : Controller
+    {
+        [HttpGet("list")]
+        public IEnumerable<Dog> GetDogs()
+        {
+            var dogs = new List<Dog>();
+
+            dogs.Add(new Dog {
+                Id = 1,
+                Legs = 4,
+                Name = "Fido",
+                Type = "Tax"
+            });
+
+            dogs.Add(new Dog
+            {
+                Id = 2,
+                Legs = 3,
+                Name = "Dino",
+                Type = "Tax"
+            });
+            dogs.Add(new Dog
+            {
+                Id = 3,
+                Legs = 4,
+                Name = "Snoopy",
+                Type = "Lion"
+            });
+
+            return dogs;
+        }
+    }
+}
